b032e0293016143fc00cb1c120abedbcd61e022f
Justin True <justin.d.true@wmich.edu>
2017-11-13 00:15:24 -0500

    fixed midnight in evening

--- resources/views/data/tablerow.blade.php
+++ resources/views/data/tablerow.blade.php
@@ -3,7 +3,7 @@
 {{-- arrays that contain the times for each time period --}}
 <?php
 	$morningTimes = array("-","7:00 AM","8:00 AM","9:00 AM","10:00 AM","11:00 AM","12:00 PM");
-	$afternoonTimes = array("-","12:00 AM","1:00 PM","2:00 PM","3:00 PM","4:00 PM","5:00 PM");
+	$afternoonTimes = array("-","12:00 PM","1:00 PM","2:00 PM","3:00 PM","4:00 PM","5:00 PM");
 	$eveningTimes = array("-","5:00 PM","6:00 PM","7:00 PM","8:00 PM","9:00 PM","10:00 PM","11:00 PM","12:00 AM","1:00 AM");
 ?>
 
